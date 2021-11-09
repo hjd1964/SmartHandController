6a79ad58e3e8a7bfe05386d155b7c71d2356532c
Howard Dutton <hjd1964@gmail.com>
2021-11-09 11:18:04 -0500

    Commented out ESP32 display hack

--- src/libApp/u8g2ext/u8g2_ext_message.h
+++ src/libApp/u8g2ext/u8g2_ext_message.h
@@ -5,8 +5,8 @@
 
 u8g2_uint_t ext_UserInterfaceMessage(u8g2_t *u8g2, KeyPad *extPad,const char *title1, const char *title2, const char *title3, const char *buttons);
 
-#ifdef ESP32
-  #define u8g2_GetAscentEx(x) (u8g2_GetAscent(x) - 3)
-#else
+//#ifdef ESP32
+//  #define u8g2_GetAscentEx(x) (u8g2_GetAscent(x) - 3)
+//#else
   #define u8g2_GetAscentEx(x) u8g2_GetAscent(x)
-#endif
+//#endif
--- src/libApp/u8g2ext/u8g2_ext_selection.h
+++ src/libApp/u8g2ext/u8g2_ext_selection.h
@@ -5,12 +5,12 @@
 
 uint8_t ext_UserInterfaceSelectionList(u8g2_t *u8g2, KeyPad *extPad, const char *title, uint8_t start_pos, const char *sl, bool wrap);
 
-#ifdef ESP32
-  #define u8g2_GetAscentEx(x) (u8g2_GetAscent(x) - 3)
-  #define u8g2_OffsetY 2
-  #define MY_SELECTION_BORDER_SIZE 0
-#else
+//#ifdef ESP32
+//  #define u8g2_GetAscentEx(x) (u8g2_GetAscent(x) - 3)
+//  #define u8g2_OffsetY 2
+//  #define MY_SELECTION_BORDER_SIZE 0
+//#else
   #define u8g2_GetAscentEx(x) u8g2_GetAscent(x)
   #define u8g2_OffsetY 0
   #define MY_SELECTION_BORDER_SIZE MY_BORDER_SIZE
-#endif
+//#endif
