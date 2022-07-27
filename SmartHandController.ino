e3443dfe5476f37de6d1055bc07fcd17e96281bf
Howard Dutton <hjd1964@gmail.com>
2022-07-27 08:30:45 -0400

    Upped minor revision

--- SmartHandController.ino
+++ SmartHandController.ino
@@ -29,8 +29,8 @@
 
 #define Product               "SHC"
 #define FirmwareVersionMajor  "3"
-#define FirmwareVersionMinor  "02"
-#define FirmwareVersionPatch  "h"
+#define FirmwareVersionMinor  "03"
+#define FirmwareVersionPatch  "a"
 
 #include "src/Common.h"
 NVS nv;
