7bc04cd8ed409c2bb29c7a6fc286842e70d33723
Howard Dutton <hjd1964@gmail.com>
2023-04-24 09:50:39 -0400

    Added ability to #define pin active state in Config.h

--- src/pinmaps/Models.h
+++ src/pinmaps/Models.h
@@ -11,13 +11,27 @@
 #include "../locales/Locale.h"
 
 // Default pin state; true for active LOW, false if active HIGH
-#define B_PIN0_ACTIVE_STATE LOW
-#define B_PIN1_ACTIVE_STATE LOW
-#define B_PIN2_ACTIVE_STATE LOW
-#define B_PIN3_ACTIVE_STATE LOW
-#define B_PIN4_ACTIVE_STATE LOW
-#define B_PIN5_ACTIVE_STATE LOW
-#define B_PIN6_ACTIVE_STATE LOW
+#ifndef B_PIN0_ACTIVE_STATE
+  #define B_PIN0_ACTIVE_STATE LOW
+#endif
+#ifndef B_PIN1_ACTIVE_STATE
+  #define B_PIN1_ACTIVE_STATE LOW
+#endif
+#ifndef B_PIN2_ACTIVE_STATE
+  #define B_PIN2_ACTIVE_STATE LOW
+#endif
+#ifndef B_PIN3_ACTIVE_STATE
+  #define B_PIN3_ACTIVE_STATE LOW
+#endif
+#ifndef B_PIN4_ACTIVE_STATE
+  #define B_PIN4_ACTIVE_STATE LOW
+#endif
+#ifndef B_PIN5_ACTIVE_STATE
+  #define B_PIN5_ACTIVE_STATE LOW
+#endif
+#ifndef B_PIN6_ACTIVE_STATE
+  #define B_PIN6_ACTIVE_STATE LOW
+#endif
 
 #include "Pins.Esp32.h"
 #include "Pins.Esp8266.h"
