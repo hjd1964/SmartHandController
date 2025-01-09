215b8f5f1e03151aef430cebfe6968d963da61bd
Howard Dutton <hjd1964@gmail.com>
2025-01-08 19:49:49 -0500

    Removed obsolete settings

--- Extended.config.h
+++ Extended.config.h
@@ -24,21 +24,3 @@
 // Warning --->                           //              ***     OR TWO THEN SET THIS OPTION TO OFF AND UPLOAD AGAIN.        ***
                                           //              ***     LEAVING THIS OPTION ENABLED CAN CAUSE EXCESSIVE NV          ***
                                           //              ***     WEAR AND DAMAGE THE MICROCONTROLLER NV SUBSYSTEM !!!        ***
-
-// IP ADDRESS SETTINGS -------------------------------------------------------------------------------------------------------------
-// these are active when SERIAL_RADIO is set to WIFI_STATION in Config.h
-
-#define ONSTEP1_HOST_NAME        "OnStep" //    "OnStep", Wifi Host Name to connnect to, leave blank to disable.              Adjust
-#define ONSTEP1_SSID             "ONSTEP" //    "ONSTEP", Wifi alternate Station SSID to connnect to.                         Adjust
-#define ONSTEP1_PASSWORD       "password" //  "password", Wifi Station mode password.                                         Adjust
-#define ONSTEP1_IP_ADDR     {192,168,0,1} //  ..168,0,1}, Wifi Target IP Address to connect to.                               Adjust
-
-#define ONSTEP2_HOST_NAME       "OnStepX" //          "", Wifi Host Name to connnect to, leave blank to disable.              Adjust
-#define ONSTEP2_SSID            "OnStepX" //    "ONSTEP", Wifi alternate Station SSID to connnect to.                         Adjust
-#define ONSTEP2_PASSWORD       "password" //          "", Wifi alternate Station mode password.                               Adjust
-#define ONSTEP2_IP_ADDR     {192,168,0,1} //  ..168,0,1}, Wifi alternate Target IP Address to connect to.                     Adjust
-
-#define ONSTEP3_HOST_NAME              "" //          "", Wifi Host Name to connnect to, leave blank to disable.              Adjust
-#define ONSTEP3_SSID             "ONSTEP" //    "ONSTEP", Wifi alternate Station SSID to connnect to.                         Adjust
-#define ONSTEP3_PASSWORD       "password" //  "password", Wifi alternate Station mode password.                               Adjust
-#define ONSTEP3_IP_ADDR     {192,168,0,1} //  ..168,0,1}, Wifi alternate Target IP Address to connect to.                     Adjust
