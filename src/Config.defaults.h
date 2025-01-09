9e304f067fc9b32e28404867fbedcbb4c4fc6542
Howard Dutton <hjd1964@gmail.com>
2025-01-09 08:48:28 -0500

    Better formatting and organization

--- src/Config.defaults.h
+++ src/Config.defaults.h
@@ -1,8 +1,18 @@
 #pragma once
 
+// host name for this microcontroller
+#ifndef HOST_NAME
+#define HOST_NAME                "OnStep-SHC"
+#endif
+
+// settings identification
+#ifndef CONFIG_NAME
+#define CONFIG_NAME              HOST_NAME
+#endif
+
 // use the HAL specified default NV driver
 #ifndef NV_DRIVER
-  #define NV_DRIVER              NV_DEFAULT
+#define NV_DRIVER                NV_DEFAULT
 #endif
 
 // activate ST4 port serial interface
@@ -14,44 +24,44 @@
 #define SERIAL_BAUD_DEFAULT      SERIAL_ONSTEP_BAUD_DEFAULT
 
 #ifndef SERIAL_RADIO
-  #define SERIAL_RADIO           OFF
+#define SERIAL_RADIO             OFF
 #endif
 
 // setup for Wifi IP communications to OnStep
 // for other settings see the WiFiManager class
 #if SERIAL_RADIO == WIFI_STATION
-  #define SERIAL_IP_MODE         STATION
+#define SERIAL_IP_MODE           STATION
 #endif
 
 // setup for Bluetooth communications to OnStep
 #if SERIAL_RADIO == BLUETOOTH
-  #define SERIAL_BT_MODE         MASTER
-  #define SERIAL_BT_NAME         "SHC"
+#define SERIAL_BT_MODE           MASTER
+#define SERIAL_BT_NAME           HOST_NAME
 #endif
 
 // setup for either WiFi or Bluetooth communications to OnStep
 #if SERIAL_RADIO == BOTH
-  #define SERIAL_IP_MODE         STATION
-  #define SERIAL_BT_MODE         MASTER
-  #define SERIAL_BT_NAME         "SHC"
+#define SERIAL_IP_MODE           STATION
+#define SERIAL_BT_MODE           MASTER
+#define SERIAL_BT_NAME           HOST_NAME
 #endif
 
 // set defaults just incase
 #ifndef SERIAL_IP_MODE
-  #define SERIAL_IP_MODE         OFF
+#define SERIAL_IP_MODE           OFF
 #endif
 
 #ifndef SERIAL_BT_MODE
-  #define SERIAL_BT_MODE         OFF
+#define SERIAL_BT_MODE           OFF
 #endif
 
 #if SERIAL_IP_MODE == STATION
-  // first, you must have an Wifi or Ethernet device:  OFF or WIFI, ETHERNET_W5100, ETHERNET_W5500
-  #define OPERATIONAL_MODE       WIFI
+// first, you must have an Wifi or Ethernet device:  OFF or WIFI, ETHERNET_W5100, ETHERNET_W5500
+#define OPERATIONAL_MODE         WIFI
 
-  // optional Arduino Serial class work-alike IP channel to port 9996 as a client (connects to a server)
-  #define SERIAL_CLIENT          ON
+// optional Arduino Serial class work-alike IP channel to port 9996 as a client (connects to a server)
+#define SERIAL_CLIENT            ON
 
-  // set station mode
-  #define STA_ENABLED            true
+// set station mode
+#define STA_ENABLED              true
 #endif
