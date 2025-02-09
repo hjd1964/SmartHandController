69f30f67733a1664419c8140ec5b75772a9673c1
Howard Dutton <hjd1964@gmail.com>
2025-02-09 09:46:08 -0500

    Upped patch level

--- SmartHandController.ino
+++ SmartHandController.ino
@@ -30,7 +30,7 @@
 #define Product               "SHC"
 #define FirmwareVersionMajor  "4"
 #define FirmwareVersionMinor  "02"
-#define FirmwareVersionPatch  "r"
+#define FirmwareVersionPatch  "s"
 
 #include "src/Common.h"
 #include "src/Validate.h"
